fc9a0b9ea724e704db208347eca1d5ccb4cee506
dayoungbbb <41858380+dayoungbbb@users.noreply.github.com>
2018-10-03 19:25:08 +0900

    BOJ 10819 차이를 최대로
    https://www.acmicpc.net/problem/10819

---
+++ MaxDifference.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+#define MAX_N 8
+int n, arr[MAX_N], visited[MAX_N], ret;
+vector<int> v;
+void bruteforce(int depth) {
+	if (depth == n) {
+		int sum = 0;
+		for (int i = 0; i < n - 1; i++) {
+			sum += abs(v[i] - v[i + 1]);
+		}
+		ret = ret > sum ? ret : sum;
+		/*if (sum > ret) {
+			cout << "sum : " << sum << ' ';
+			for (int j = 0; j < n; j++)
+				cout << v[j] << ' ';
+			cout << '\n';
+			ret = sum;
+		}*/
+	}
+	for (int i = 0; i < n; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			v.push_back(arr[i]);
+			bruteforce(depth+1);
+			visited[i] = false;
+			v.pop_back();
+		}
+	}
+}
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	ret = 0;
+	bruteforce(0);
+	printf("%d", ret);
+}
