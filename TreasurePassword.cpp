44ef60729e400679e0fcb1cbf4ee31069a58e0d3
dayoungbbb <41858380+dayoungbbb@users.noreply.github.com>
2018-10-16 01:53:59 +0900

    #SWEA 5658. [모의 SW 역량테스트] 보물상자 비밀번호
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXRUN9KfZ8DFAUo

--- TreasurePassword.cpp
+++ TreasurePassword.cpp
@@ -2,14 +2,11 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <limits.h>
 #include <functional>
 using namespace std;
-int n, k;
-bool visited[LONG_MAX];
 int main()
 {
-	int tc; cin >> tc;
+	int tc, n, k; cin >> tc;
 	for (int t = 1; t <= tc; t++) {
 		cin >> n >> k;
 		string input; cin >> input;
@@ -17,24 +14,24 @@ int main()
 		vector<string> v;
 		for (int i = 0; i < cut; i++) {
 			for (int k = 0; k < 4; k++) {
-				string numStr; int num =0;
-				for (int j = 0; j < cut; j++) {
+				string numStr; int num = 0;
+				for (int j = 0; j < cut; j++) 
 					numStr += input[k*cut + j];
-					if (input[k*cut+j] >= 'A') num = num*16 + (input[k*cut+j] - 'A' + 10);
-					else num = num*16 + (input[k*cut+j] - '0');
+				bool chk = true;
+				for (int j = 0; j < (int)v.size(); j++) {
+					if (v[j] == numStr)
+						chk = false;
 				}
-				if (!visited[num]) {
-					visited[num] = true;
+				if (chk)
 					v.push_back(numStr);
-				}
 			}
-			input = input[n - 1] + input.substr(0, n-1);
+			input = input[n - 1] + input.substr(0, n - 1);
 		}
 		sort(v.begin(), v.end(), greater<string>());
 		long long num = 0;
 		for (int i = 0; i < cut; i++)
-			if (v[k-1][i] >= 'A') num = num * 16 + (v[k-1][i] - 'A' + 10);
-			else num = num * 16 + (v[k-1][i] - '0');
+			if (v[k - 1][i] >= 'A') num = num * 16 + (v[k - 1][i] - 'A' + 10);
+			else num = num * 16 + (v[k - 1][i] - '0');
 		cout << '#' << t << ' ' << num << '\n';
 	}
 	return 0;
